407110cffba605966858f4f8f4f11a273d8d707d
Sequence-94 <53806574+Sequence-94@users.noreply.github.com>
2024-02-12 20:03:45 +0200

    update on serializer pattern

--- Software_Review_GUI/main.cpp
+++ Software_Review_GUI/main.cpp
@@ -1,9 +1,15 @@
 #include <QApplication>
-#include"softwaregui.h"
 #include"software.h"
 #include"softwarelist.h"
+#include"softwaregui.h"
+#include"listreader.h"
+#include"listwriter.h"
 #include<QDate>
 #include<QString>
+#include<QTextStream>
+
+
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
@@ -12,10 +18,22 @@ int main(int argc, char *argv[])
     Software soft1("Marvel",d1,true);
     Software soft2("Star Wars",d2,true);
 
-    SoftwareList list;
-    list.add(soft1);
-    list.add(soft2);
+    SoftwareList list1;
+    list1.add(soft1);
+    list1.add(soft2);
 
+    ListWriter lw;
+    lw.write(list1);
+
+    SoftwareList list2;
+    ListReader lr;
+    list2 = lr.read();
+
+    QTextStream cout(stdout);
+    QList<Software> sl = list2.getSoftList();
+    foreach(Software s,sl){
+        cout<<s.getName()<<": "<<s.getDate().toString("dd-MM-yyyy")<<": "<<QVariant(s.getRecommend()).toString()<<"\n";
+    }
     SoftwareGui software;
     software.show();
 
